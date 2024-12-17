0a7fd05c6e4bc7fc3204864fdde1feebc79a6633
azad032 <this.azad@yahoo.com>
2024-12-17 23:57:22 +0600

    class 9

---
+++ bin/class_9/abstract.dart
@@ -0,0 +1,38 @@
+abstract class Animal{
+  String name;
+  static var test = "this is animal";
+
+  Animal(this.name);
+
+  void eat(){
+    print("Animal is eating");
+  }
+
+  void speak(){
+    print("Animal is speaking");
+  }
+}
+
+
+class Dog extends Animal{
+  String color;
+
+  Dog(this.color):super('Tom');
+
+
+}
+
+
+main(){
+
+  Dog tom = Dog('Red');
+
+  print(Animal.test);
+
+  tom.eat();
+  tom.speak();
+
+
+
+
+}
---
+++ bin/class_9/inheritence.dart
@@ -0,0 +1,62 @@
+import 'abstract.dart';
+class Father{
+  String name;
+  String land = '100 bigha';
+  String hou = "Tin basa";
+  String bike = "Hero Honda";
+
+  Father(this.name);
+
+  void incomeSource(){
+    print("From farming");
+  }
+
+  void MySelf(){
+    print(name);
+  }
+}
+
+
+class Son extends Father{
+  String name;
+  Son(this.name):super('Rahim');
+
+  @override
+ void incomeSource(){
+    print("App developer");
+  }
+
+  @override
+  void MySelf(){
+    print("My Father name is: ");
+    super.MySelf();
+    print(" My name is $name");
+  }
+}
+
+
+main(){
+
+  Son Rafi = Son('Rafi');
+  Father Rahim = Father('Rahim');
+  Rafi.hou = "Sad er basa";
+  Rafi.land = "10 bigha";
+
+
+  print(Rafi.hou);
+  print(Rafi.land);
+  print(Rahim.land);
+
+
+  Rafi.incomeSource();
+
+
+
+
+  print("i have ${Rafi.bike}");
+
+  Rafi.MySelf();
+
+  Animal.test;
+
+}
